82f3ab9501607ecee3c189f42cc7f1f3c403ff28
mandaramsusmi-tech <mandaramsusmi@gmail.com>
2026-01-28 23:19:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+function openMenu(){
+
+navLinks.classList.toggle("active");     
+   
+}
