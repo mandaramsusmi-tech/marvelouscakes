8a889c4198f70539ecc0cab15f2aa28f689c7c87
mandaramsusmi-tech <mandaramsusmi@gmail.com>
2026-02-08 22:02:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 
 const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("navLinks");
-
 function openMenu(){
-
-navLinks.classList.toggle("active");     
-   
+document.getElementById("navLinks").classList.toggle("active");    
 }
+const contactForm=
+document.getElementById("contactform"); 
+if("contactform") {
+cotactForm.addEventListener("submit", function(e){
+    e.preventDefault();
+});
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let whatsappMessage = 
+      "Name: " + name + "%0A" +
+       "Email: " + email + "%0A" +
+        "Message: " + message;
+    let phoneNumber = "918129981952";
+    window.open(
+        "https//wa.me/" + phoneNumber + "? text=" + whatsappMessage,"_blank"
+    );
+};
+    function toggleContact() {
+        const policies = document.getElementById("mobilePolicies");
+        const icon = document.getElementById("icon");
+        
+          if (policies.style.display ==="flex"){
+            policies.style.display = "none";
+    icon.classList.replace("fa-plus","fa-minus");
+          } else {
+            policies.style.display = "flex";
+    icon.classList.replace("fa-minus","fa-plus");
+    }
+       
+    }
+   
+
+
